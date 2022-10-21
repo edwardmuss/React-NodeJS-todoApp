3b35fc638fa1bd74c5567d89d40a397eebc39e28
edwardmuss <edwardmuss5@.com>
2022-10-21 17:53:34 +0300

    Configured serveless

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'vbg012vnd6'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-n8tk2y3o4xtmi5dw.us.auth0.com',            // Auth0 domain
+  clientId: 'OxuMgw9cuuuFGgagaoFTQbEhQ8LnmM5L',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
